857a6467c1d35f9f3f33df57493dd4078c0aa548
sebastianmel <57873668+sebastianmel@users.noreply.github.com>
2021-11-19 15:43:21 +0100

    step 3
    page game modes

--- src/pages/Tft.js
+++ src/pages/Tft.js
@@ -13,12 +13,9 @@ const Tft = () => {
             <div className="rankContain">
                 <div className="rankLeft">
                     <br />
-                    <p>Même les joueurs professionnels de la Ligue doivent commencer quelque part, et si beaucoup jouent depuis les premières saisons, de nouveaux joueurs font constamment irruption sur la scène. Examinons de plus près les rank LoL.</p>
+                    <p>Teamfight Tactics (abrégé TFT, parfois Combat Tactique (abrégé CT) en français) est un jeu vidéo de type auto battler (en) développé et édité par Riot Games. Il prend place dans l'univers de League of Legends et est basé sur le jeu Dota Auto Chess (en), où le joueur affronte sept adversaires en ligne, qu'il doit combattre en formant une équipe afin d'être le dernier à rester en vie.</p>
                     <br />
-                    <p>Mais ces joueurs ne trouvent pas leur chemin vers les équipes professionnelles après avoir joué à l’université ou dans les mineures. Chaque pro de League of Legends a parcouru les échelles de classement jusqu’à Maître, Grand Maître, et Challenger, les niveaux de classement finaux très compétitifs.</p>
-                    <br />
-                    <p>Cependant, cela peut être une perspective décourageante pour les nouveaux venus dans LoL compétitif. Beaucoup ont des questions persistantes, comme : Combien y a-t-il de rangs, comment s’appellent-ils, dans lequel vais-je commencer, et combien de personnes se retrouvent dans le premier rang ? Pour simplifier un peu ta transition dans le monde de LoL compétitif, nous avons rassemblé quelques réponses aux questions les plus fréquentes sur le système de classement de League of Legends.</p>
-                    <br />
+                   
                 </div>
                 <div className="rankRigth">
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/m2z9buDYVa4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>

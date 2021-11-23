4d022ca6d1b8777c7023bf20100758bb8e2b8d85
sebastianmel <57873668+sebastianmel@users.noreply.github.com>
2021-11-23 22:14:25 +0100

    no add

--- src/pages/Tft.js
+++ src/pages/Tft.js
@@ -23,14 +23,7 @@ const Tft = () => {
                     <br />
                     <br />
                     <p>Teamfight Tactics (abrégé TFT, parfois Combat Tactique (abrégé CT) en français) est un jeu vidéo de type auto battler (en) développé et édité par Riot Games. Il prend place dans l'univers de League of Legends et est basé sur le jeu Dota Auto Chess (en), où le joueur affronte sept adversaires en ligne, qu'il doit combattre en formant une équipe afin d'être le dernier à rester en vie.</p>
-                    <br />
-                    <br />
-                    <p>Teamfight Tactics (abrégé TFT, parfois Combat Tactique (abrégé CT) en français) est un jeu vidéo de type auto battler (en) développé et édité par Riot Games. Il prend place dans l'univers de League of Legends et est basé sur le jeu Dota Auto Chess (en), où le joueur affronte sept adversaires en ligne, qu'il doit combattre en formant une équipe afin d'être le dernier à rester en vie.</p>
-                    <br />
-                    <br />
-                    <p>Teamfight Tactics (abrégé TFT, parfois Combat Tactique (abrégé CT) en français) est un jeu vidéo de type auto battler (en) développé et édité par Riot Games. Il prend place dans l'univers de League of Legends et est basé sur le jeu Dota Auto Chess (en), où le joueur affronte sept adversaires en ligne, qu'il doit combattre en formant une équipe afin d'être le dernier à rester en vie.</p>
-                    <br />
-                   
+                    
                 </div>
                 <div className="rankRigth">
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/m2z9buDYVa4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
